c0c7ec0052f881a90a3df674bc715c2e55edca1c
Alan Gonzalez <alanboy@alanboy.net>
2016-11-17 02:21:41 -0800

    Agregar usuarios para el selectivo de Quintana Roo
    Pablo Batun Salazar necesita crear usuarios para su selectivo.

--- frontend/server/controllers/UserController.php
+++ frontend/server/controllers/UserController.php
@@ -878,14 +878,24 @@ class UserController extends Controller {
             $keys = array(
                 'CCUPITSUR-16' => 50,
             );
-        } elseif ($r['contest_type'] == 'CONALEP') {
-            if ($r['current_user']->username != 'reyes811'
+        } elseif ($r['contest_type'] == 'CCUPITSUR') {
+            if ($r['current_user']->username != 'licgerman-yahoo'
+                && !$is_system_admin
+            ) {
+                throw new ForbiddenAccessException();
+            }
+            // Arreglo de concurso
+            $keys = array(
+                'CCUPITSUR-16' => 50,
+            );
+        } elseif ($r['contest_type'] == 'OMIQROO') {
+            if ($r['current_user']->username != 'pablobatun'
                 && !$is_system_admin
             ) {
                 throw new ForbiddenAccessException();
             }
             $keys = array (
-                'OIC-16' => 225
+                'ROO17-' => 160
             );
         } else {
             throw new InvalidParameterException(
@@ -893,7 +903,7 @@ class UserController extends Controller {
                 'contest_type',
                 array(
                     'bad_elements' => $r['contest_type'],
-                    'expected_set' => 'OMI, OMIAGS, ORIG, OSI, OVI, PROFEST, CCUPITSUR, CONALEP',
+                    'expected_set' => 'OMI, OMIAGS, ORIG, OSI, OVI, PROFEST, CCUPITSUR, CONALEP, OMIQROO',
                 )
             );
         }
